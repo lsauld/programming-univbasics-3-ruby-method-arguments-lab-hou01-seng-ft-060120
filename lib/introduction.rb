1121d9226c186a318f34b51539d01ad9ebcb8311
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-05-06 20:44:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,11 +9,14 @@ def introduction_with_language(name, language)
 end
 
 
-def introduction_with_language_optional(name, language)
-
-
+def introduction_with_language_optional(name, language = "Ruby")
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
 end
+
+
+
+
 
 
 
